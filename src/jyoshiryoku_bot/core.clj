6670f929434f6272bee75d76ad6ac16687057e95
syobochim <MarilynMonroe0226@gmail.com>
2014-10-03 12:47:20 +0900

    メンションを取得する

--- src/jyoshiryoku_bot/core.clj
+++ src/jyoshiryoku_bot/core.clj
@@ -1,7 +1,16 @@
 (ns jyoshiryoku-bot.core
   (:import [twitter4j TwitterFactory Twitter]))
 
-(defn userinfo [message]
+(defn tweettimeline [message]
   (let [twitter (.getInstance (TwitterFactory.)) , user (.verifyCredentials twitter)]
     (.updateStatus twitter message)
     ))
+
+(defn mymentionlist []
+  (.getMentionsTimeline (.getInstance (TwitterFactory.))))
+
+(defn getmymention []
+  (let [twitter (.getInstance (TwitterFactory.))]
+    (mymentionlist)
+    ))
+
