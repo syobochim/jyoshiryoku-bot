9b47a5a7ce0c72a6a23e790d5da236f415cefdba
syobochim <MarilynMonroe0226@gmail.com>
2014-10-04 22:24:27 +0900

    Kuromojiをリポジトリに追加

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject jyoshiryoku-bot "0.1.0-SNAPSHOT"
   :description "女子力のたかまるツイートをする"
+  :repositories {"Atilika Open Source repository" "http://www.atilika.org/nexus/content/repositories/atilika"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.twitter4j/twitter4j-core "[4.0,)"]])
+                 [org.twitter4j/twitter4j-core "[4.0,)"]
+                 [org.atilika.kuromoji/kuromoji "0.7.7"]])
