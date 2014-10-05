20db47dd988c4ff91a3512663f8387e76bd35bd3
syobochim <MarilynMonroe0226@gmail.com>
2014-10-05 17:44:07 +0900

    メンションの情報をシーケンスで取得

--- src/jyoshiryoku_bot/core.clj
+++ src/jyoshiryoku_bot/core.clj
@@ -1,23 +1,33 @@
 (ns jyoshiryoku-bot.core
   (:import [twitter4j TwitterFactory Twitter Paging])
-  (:require [clojure.contrib.str-utils :as str-utils]))
+  (:require [clojure.contrib.str-utils :as str-utils]
+            [jyoshiryoku-bot.kaiseki :as kaiseki])
+  (:gen-class))
 
 (defn mytwitter []
   (.getInstance (TwitterFactory.)))
 
 (defn tweettimeline [message]
   (let [twitter (mytwitter) , user (.verifyCredentials twitter)]
-    (.updateStatus twitter message)
-    ))
+    (.updateStatus twitter message)))
 
-(defn mymentionlist []
-  (.getMentionsTimeline (mytwitter)))
+(defn mymention []
+  (lazy-seq (.getMentionsTimeline (mytwitter))))
 
+(defn resentmention [] (first (mymention)))
+
+(defn mentionInfo []
+    (let [mentionUser (.getScreenName (.getUser (resentmention)))
+          mentionText (str-utils/re-sub #"@syobochim\s" "" (.getText (resentmention)))]
+      {:userName mentionUser :text mentionText}))
 
 (def paging
-  (Paging. (Integer. 1) (Integer. 200)))
+ (Paging. (Integer. 1) (Integer. 5)))
 
 (defn getmytweet []
   (let [twitter (mytwitter)]
-     (map #(. %1 getText) (.getUserTimeline twitter paging))))
+     (map #(. %1 getText) (.getUserTimeline twitter))))
 
+(kaiseki/init getmytweet)
+
+(println (kaiseki/create-sentence @kaiseki/*words* (:text (mentionInfo))))
